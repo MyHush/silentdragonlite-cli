d94681c32ec169bfbfb789f9bdee06a9854620ec
DenioD <41270280+DenioD@users.noreply.github.com>
2020-10-04 10:02:00 +0200

    add spent height

--- lib/src/lightclient.rs
+++ lib/src/lightclient.rs
@@ -790,6 +790,8 @@ impl LightClient {
                                 "address"            => address,
                                 "spendable"          => spendable,
                                 "spent"              => nd.spent.map(|spent_txid| format!("{}", spent_txid)),
+                                "spent_at_height"    => nd.spent_at_height.map(|h| format!("{}", h)),
+                                "witness_size"       => nd.witnesses.len(),
                                 "unconfirmed_spent"  => nd.unconfirmed_spent.map(|spent_txid| format!("{}", spent_txid)),
                             })
                         }
